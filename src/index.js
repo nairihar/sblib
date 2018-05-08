fb79f94879d3c44579ae7386a3054cc30b0f3484
Nairi <nairihar99@gmail.com>
2018-05-08 15:09:29 +0400

    add sdk folder

--- src/index.js
+++ src/index.js
@@ -1,34 +1,5 @@
-import Http from './http'
+import BSDK from './sdk'
 
-const _privates = new WeakMap()
+export default BSDK
 
-export default class BSDK {
-  constructor({ name='', version='' }) {
-    const _state = {
-      info: {
-        name,
-        version
-      }
-    }
-    _privates.set(this, _state)
-  }
-
-  /* getters */
-  getInfo() {
-    const { info } = _privates.get(this)
-    return info
-  }
-
-  getName() {
-    const { name } = this.getInfo()
-    return name
-  }
-
-  getVersion() {
-    const { version } = this.getInfo()
-    return version
-  }
-}
-
-
-export { Http }
+export { default as Http } from './http'
