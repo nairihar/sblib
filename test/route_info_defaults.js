7882a7531315da812ef1001838b2c5d658e5091b
Nairi <nairihar99@gmail.com>
2018-05-13 20:42:19 +0400

    fix test

--- test/route_info_defaults.js
+++ test/route_info_defaults.js
@@ -18,7 +18,7 @@ describe('Route info defauls', () => {
       assert.equal(api.getAddress(), slicedAddress)
     })
     it('getUrl should return address', () => {
-      assert.equal(api.getUrl(), slicedAddress)
+      assert.equal(api.getUrl(), address)
     })
     it('getUrl should return "/"', () => {
       assert.equal(api.getPath(), defaults.path)
